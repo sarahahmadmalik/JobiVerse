823e6e187e738a87fc28ef3dabc6ea97c702cfb0
Sara Ahmad Malik <sarahahmadmalik55@gmail.com>
2025-03-24 02:22:07 +0500

    fix(vercel): deployement issue resolved, import for context folder fixed

--- src/app/(onboarding)/onboarding/page.jsx
+++ src/app/(onboarding)/onboarding/page.jsx
@@ -1,6 +1,6 @@
 "use client";
-import { OnboardingProvider } from "@/contexts/OnboardingContext/OnboardingContext";
-import OnboardingWrapper from "@/components/onboarding/OnboardingWrapper";
+import { OnboardingProvider } from "@/contexts/OnBoardingContext/OnboardingContext";
+import OnboardingWrapper from "@/components/onBoarding/OnboardingWrapper";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 
